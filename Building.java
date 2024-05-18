1e14cfad0835935636e5ff98bdb3c4b770b7148d
SlowpokerFace <112822397+SlowpokerFace@users.noreply.github.com>
2024-05-18 05:59:11 +0300

    Add files via upload

--- Building.java
+++ Building.java
@@ -15,29 +15,26 @@ public class Building {
     }
 
     public void chooseElevator(Resident passenger) {
-        if (elevators.getFirst().direction == 0 && elevators.getLast().direction == 0) {
+        if (elevators.getFirst().currentFloor == passenger.from_floor){
+            passenger.entered = true;
+            elevators.getFirst().addPassenger(passenger);
+        }
+        else if (elevators.getLast().currentFloor == passenger.from_floor){
+            passenger.entered = true;
+            elevators.getLast().addPassenger(passenger);
+        }
+        else if (elevators.getFirst().direction == 0 && elevators.getLast().direction == 0) {
             if (Math.abs(elevators.getFirst().currentFloor - passenger.from_floor) <= Math.abs(elevators.getLast().currentFloor - passenger.from_floor)) {
                 elevators.getFirst().addPassenger(passenger);
             } else {
                 elevators.getLast().addPassenger(passenger);
             }
-        } else if (elevators.getFirst().direction != 0 && elevators.get(1).direction == 0) {
+        } else if (elevators.getFirst().direction != 0 && elevators.getLast().direction == 0) {
             elevators.getLast().addPassenger(passenger);
-        } else if (elevators.getFirst().direction == 0 && elevators.get(1).direction != 0) {
+        } else if (elevators.getFirst().direction == 0 && elevators.getLast().direction != 0) {
             elevators.getFirst().addPassenger(passenger);
-        } else if (Math.max(elevators.getFirst().passengers.peek().from_floor, elevators.getFirst().passengers.peek().to_floor) >= passenger.from_floor && Math.min(elevators.getFirst().passengers.peek().from_floor, elevators.getFirst().passengers.peek().to_floor) <= passenger.from_floor) {
-            elevators.getFirst().addPassenger(passenger);
-        } else if (Math.max(elevators.getLast().passengers.peek().from_floor, elevators.getLast().passengers.peek().to_floor) >= passenger.from_floor && Math.min(elevators.getLast().passengers.peek().from_floor, elevators.getLast().passengers.peek().to_floor) <= passenger.from_floor) {
-            elevators.getLast().addPassenger(passenger);
-        } else if (Math.abs(elevators.getFirst().passengers.peek().to_floor - passenger.from_floor) <= Math.abs(elevators.getLast().passengers.peek().to_floor - passenger.from_floor))
-        {
-            elevators.getFirst().addPassenger(passenger);
+        }
 
-        }
-        else if (Math.abs(elevators.getFirst().passengers.peek().to_floor - passenger.from_floor) > Math.abs(elevators.getLast().passengers.peek().to_floor - passenger.from_floor))
-        {
-            elevators.getLast().addPassenger(passenger);
-        }
         else{
             elevators.getLast().addPassenger(passenger);
         }
